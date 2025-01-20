97bf20ea94bff6ff3e8378ed88a4c4d6bc4ee921
Amogh Huddar <amoghhuddar@N0315-Manasa.local>
2025-01-20 14:31:35 +0530

    Changed mappings from @Getmapping to @RequestMapping. Although the latter is the older version, we use it. To be rechecked

--- server/src/main/java/com/increff/server/controller/ClientController.java
+++ server/src/main/java/com/increff/server/controller/ClientController.java
@@ -1,22 +1,16 @@
 package com.increff.server.controller;
 
-// import java.util.ArrayList;
+
 import java.util.List;
-// import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.annotations.Api;
 
 import com.increff.commons.model.ClientData;
 import com.increff.commons.model.ClientForm;
-// import com.increff.server.api.ClientApi;
-// import com.increff.server.entity.Client;
 import com.increff.commons.exception.ApiException;
 import com.increff.server.dto.ClientDto;
-
-import io.swagger.annotations.Api;
-// import io.swagger.annotations.ApiOperation;
 
 @Api
 @RestController
@@ -26,23 +20,24 @@ public class ClientController {
     @Autowired
     private ClientDto dto;
 
-    @PostMapping
+
+    @RequestMapping(method = RequestMethod.POST)
     public void add(@RequestBody ClientForm form) throws ApiException {
         //@RequestBody is used to bind the request body to the form object
         dto.add(form);
     }
 
-    @GetMapping
+    @RequestMapping(method = RequestMethod.GET)
     public List<ClientData> getAll() {
         return dto.getAll();
     }
 
-    @GetMapping("/{id}")
+    @RequestMapping(method = RequestMethod.GET, value = "/{id}")
     public ClientData get(@PathVariable int id) throws ApiException {
         return dto.get(id);
     }
 
-    @PutMapping("/{id}")
+    @RequestMapping(method = RequestMethod.PUT, value = "/{id}")
     public void update(@PathVariable int id, @RequestBody ClientForm form) throws ApiException {
         dto.update(id, form);
     }
